a26a38bf94fdd91b2fbfca24eaa6f965905ce3af
wx85745997 <wx85745997@gmail.com>
2025-07-06 18:57:15 +0800

    04:background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,47 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Client } from "./client";
-import { Suspense } from "react";
+// import { getQueryClient, trpc } from "@/trpc/server";
+// import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+// import { Client } from "./client";
+// import { Suspense } from "react";
+"use client";
 
-const Page = async () => {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(
-    trpc.createAI.queryOptions({ text: "Antonio PRE" })
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+// const Page = async () => {
+//   const queryClient = getQueryClient();
+//   void queryClient.prefetchQuery(
+//     trpc.createAI.queryOptions({ text: "Antonio PRE" })
+//   );
+
+//   return (
+//     <HydrationBoundary state={dehydrate(queryClient)}>
+//       <Suspense fallback={<p>Loading...</p>}>
+//         <Client />
+//       </Suspense>
+//     </HydrationBoundary>
+//   );
+// };
+
+const Page = () => {
+  const trpc = useTRPC();
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success("job");
+      },
+    })
   );
-
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Client />
-      </Suspense>
-    </HydrationBoundary>
+    <div>
+      <Button
+        disabled={invoke.isPending}
+        onClick={() => invoke.mutate({ text: "123 " })}
+      >
+        test
+      </Button>
+    </div>
   );
 };
 
