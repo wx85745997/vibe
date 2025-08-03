a78d4286db3928a890db45f602e6cd1b5f99ce69
wx85745997 <wx85745997@gmail.com>
2025-08-03 12:18:39 +0800

    05: ai jobgs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,10 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
 // const Page = async () => {
@@ -25,6 +27,7 @@ import { toast } from "sonner";
 // };
 
 const Page = () => {
+  const [value, setValue] = useState("");
   const trpc = useTRPC();
   const invoke = useMutation(
     trpc.invoke.mutationOptions({
@@ -35,9 +38,10 @@ const Page = () => {
   );
   return (
     <div>
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
       <Button
         disabled={invoke.isPending}
-        onClick={() => invoke.mutate({ text: "123 " })}
+        onClick={() => invoke.mutate({ value })}
       >
         test
       </Button>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,33 @@
+import { deepseek } from "@inngest/ai/models";
 import { inngest } from "./client";
+import { createAgent } from '@inngest/agent-kit';
 
 export const helloWorld = inngest.createFunction(
     { id: "hello-world" },
     { event: "test/hello.world" },
     async ({ event, step }) => {
-        await step.sleep("wait-a-moment", "5s");
-        await step.sleep("wait-a-moment", "1s");
-        return { message: `Hello ${event.data.email}!` };
+
+        const codeAgent = createAgent({
+            name: 'codeAgent',
+            system: '你是一位精通 Next.js 的专家开发者，擅长编写可读性强、易于维护的代码，能够高效输出简洁的 Next.js 和 React 示例代码。',
+            model: deepseek({
+                apiKey: process.env.DEEPSEEK_API_KEY!,
+                model: "deepseek-chat",
+            }),
+        });
+
+        console.log("1")
+
+
+        const { output } = await step.run("call-codeAgent", async () => {
+            return await codeAgent.run(
+                `请根据以下描述编写代码片段：${event.data.value}`
+            );
+        });
+
+        console.log("output", output);
+        return {
+            output,
+        };
     },
 );
